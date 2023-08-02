5221564a1df67b5e5c9f021dfcf714a3e519ef55
iamdumonde <martinaboumonde@gmail.com>
2023-08-02 17:18:54 +0100

    Création de fichier ts pour les class

---
+++ src/app/mock-infos.ts
@@ -0,0 +1,37 @@
+import { Categorie } from "./class-infos";
+
+
+export const CATEGORIE: Categorie[] = [
+    {
+        "id": "1",
+        "titre": "loreme"
+    },
+    {
+        "id": "1",
+        "titre": "loreme"
+    },
+    {
+        "id": "1",
+        "titre": "loreme"
+    },
+    {
+        "id": "1",
+        "titre": "loreme"
+    },
+    {
+        "id": "1",
+        "titre": "loreme"
+    },
+    {
+        "id": "1",
+        "titre": "loreme"
+    },
+    {
+        "id": "1",
+        "titre": "loreme"
+    },
+    {
+        "id": "1",
+        "titre": "loreme"
+    },
+]
