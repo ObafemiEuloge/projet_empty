0e04c50bb0a895347a715c492c0a539ed65fcd07
Singor Kpatchou <kpatchousingor1@gmail.com>
2023-08-02 17:13:01 +0100

    Création du chatService et importation dans le chat.component.ts

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../chat.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  constructor(
+    private chatService: ChatService,
+  ){}
+  
+  ngOnInit(): void {
+    
+  }
 
 }
