822fc24b94ce014a7a3cb4185a7cd3eb72c7c225
Sam-Rod LAHAMI <lahamirod@gmail.com>
2023-08-02 18:25:05 +0200

    les themes

---
+++ src/app/mock-infos.ts
@@ -0,0 +1,53 @@
+export const THEME_LISTS: Theme[] = [
+    {
+        "id": "1",
+        "theme_title": ["lorem",
+            "ipsum",
+            "dolor",
+            "sit",
+            "amet",
+            "consectetur",
+            "adipisicing",
+            "elit",
+            "Delectus",
+            "ut",
+        ]
+    },
+    {
+        "id": "2",
+        "theme_title": ["lorem"]
+    },
+    {
+        "id": "3",
+        "theme_title": ["lorem"]
+    },
+    {
+        "id": "4",
+        "theme_title": ["lorem"]
+    },
+    {
+        "id": "5",
+        "theme_title": ["lorem"]
+    },
+    {
+        "id": "6",
+        "theme_title": ["lorem"]
+    },
+    {
+        "id": "7",
+        "theme_title": ["lorem"]
+    },
+    {
+        "id": "8",
+        "theme_title": ["lorem"]
+    },
+    {
+        "id": "9",
+        "theme_title": ["lorem"]
+    },
+    {
+        "id": "10",
+        "theme_title": ["lorem"]
+    }
+
+]
