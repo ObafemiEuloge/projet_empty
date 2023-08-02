12d88c332b8a870bce44bad59020e4331dd6ea42
iamdumonde <martinaboumonde@gmail.com>
2023-08-02 16:23:02 +0100

    Création des composants Chat et Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { TemplateComponent } from './template/template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
