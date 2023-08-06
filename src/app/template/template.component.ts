a3b98f9a32945f023e23675345bf0aa7f98d6a17
AGNOUN Obafèmi Euloge <128822973+ObafemiEuloge@users.noreply.github.com>
2023-08-07 01:44:24 +0200

    euloge

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { opacityAnimation } from '../animation.module';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css'],
+  animations: [opacityAnimation]
 })
 export class TemplateComponent {
 
