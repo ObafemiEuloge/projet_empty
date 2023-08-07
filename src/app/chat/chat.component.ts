a8526cd804bc076810c94aa10efadd60b3759e6b
Singor Kpatchou <kpatchousingor1@gmail.com>
2023-08-07 18:03:14 +0100

    ajout 3

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../chat.service';
-import { Categorie, Questions } from '../class-infos';
+import { Categorie, Questions, quizAndAnswer } from '../class-infos';
 import { opacityAnimation } from '../animation.module';
 import { CATEGORIE, chatBotQuiz } from '../mock-infos';
 import { FormControl, FormGroup } from '@angular/forms';
@@ -16,18 +16,22 @@ export class ChatComponent implements OnInit {
   categories: Categorie[] = CATEGORIE; // récupérer le tableau stockant les catégories 
   quizsList: string[] | undefined;  // récupérer le tableau de question 
   answersList: string[] | undefined;  // récupérer le tableau de réponses 
-  quizsAndAnswersList: string[] | undefined;  // récupérer le tableau de (questions et réponses) 
+  quizsAndAnswersList: any[] | undefined;  // récupérer le tableau de (questions et réponses) 
 
   titleSite: string = "";  // récupérer le nom du site de l'utilisateur
   categ: string = "";  // récupérer la catégorie de l'utilisateur
   them: string = "";  // récupérer le thème de l'utilisateur
+  color: string = "#526926";  // récupérer le thème de l'utilisateur
+  colorArray: string[] | undefined = [];  // récupérer les couleurs de l'utilisateur
 
+  pColor!: string | null;
 
   input: boolean = false;
   inputThem: boolean = false;
   selectCat: boolean = false;
   selectThem: boolean = false;
   selectColor: boolean = false;
+  buttonSendMsg: boolean = true;
 
   constructor(
     private chatService: ChatService
@@ -36,6 +40,7 @@ export class ChatComponent implements OnInit {
   ngOnInit(): void {
     this.quizsAndAnswersList = this.chatService.getQuizAndAnswers()
     this.input = true;
+
   }
   click: number = 0
   sendNewQuiz() {
@@ -59,17 +64,21 @@ export class ChatComponent implements OnInit {
       this.quizsAndAnswersList = this.chatService.getQuizAndAnswers()
       this.input = false;
       this.selectCat = false;
-      this.selectThem = false;
-      this.selectColor = true;
+      this.inputThem = false;
       this.click = 3
+      this.selectColor = true;
+      this.buttonSendMsg = false
     }
-    
+    if (this.color !== "" && this.click === 3) {
+      this.answersList = this.chatService.getAnswers(this.them)
+      this.quizsAndAnswersList = this.chatService.getQuizAndAnswers()
+      this.input = false;
+      this.selectCat = false;
+      this.selectThem = false;
+      this.click = 4
+    }
+
   }
-  
-  // writeTheme(){
-  //   this.selectThem = false;
-  //   this.inputThem = true;
-  // }
 
   onSelectCategorie(categorie_id: any) {
     this.chatService.getCategorieThemeList(categorie_id.value);
@@ -90,7 +99,15 @@ export class ChatComponent implements OnInit {
 
   });
 
-  onSubmit() { }
-  
+  onSubmit() {
+    this.colorArray = []
+    let pColor = this.profileForm.value.primaryColor
+    let sColor = this.profileForm.value.secondaryColor
+    let tColor = this.profileForm.value.tertiaryColor
+    this.colorArray?.push(pColor!, sColor!, tColor!)
+    this.quizsAndAnswersList?.push(this.colorArray);
+    console.log(this.quizsAndAnswersList);
+  }
+
 }
 
