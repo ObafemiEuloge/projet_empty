50be690d99ddefafb328c290c66a3a63ac34e156
Euloge <eagnoun@gmail.com>
2023-08-07 10:46:44 +0100

    Conflit réglé

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -17,9 +17,8 @@ export class ChatComponent implements OnInit {
   answersList: string[] | undefined;  // récupérer le tableau de réponses 
   quizsAndAnswersList: string[] | undefined;  // récupérer le tableau de (questions et réponses) 
 
-  titleSite: string = "";  // récupérer le nom du site de l'utilisateur
-  categ: string = "";  // récupérer la catégorie de l'utilisateur
-  them: string = "";  // récupérer le thème de l'utilisateur
+  titleSite: string = "";  // récupérer la réponse de l'utilisateur
+  cate: string = "";  // récupérer la réponse de l'utilisateur
 
   input: boolean = false;
   selectCat: boolean = false;
@@ -33,33 +32,21 @@ export class ChatComponent implements OnInit {
     this.quizsAndAnswersList = this.chatService.getQuizAndAnswers()
     this.input = true;
   }
-  click: number = 0
   sendNewQuiz() {
-    if (this.titleSite !== "" && this.click === 0) {
+    if (this.titleSite !== "") {
       this.answersList = this.chatService.getAnswers(this.titleSite)
       this.quizsAndAnswersList = this.chatService.getQuizAndAnswers()
-      this.input = false;
-      this.selectCat = true;
-      this.click = 1;
     }
-    if (this.categ !== "" && this.click === 1) {
-      this.answersList = this.chatService.getAnswers(this.categ)
-      this.quizsAndAnswersList = this.chatService.getQuizAndAnswers()
-      this.input = false;
-      this.selectCat = false;
-      this.selectThem = true;
-      this.click = 2
-    }
-    if (this.them !== "" && this.click === 2) {
-      this.answersList = this.chatService.getAnswers(this.them)
-      this.quizsAndAnswersList = this.chatService.getQuizAndAnswers()
-      this.click = 3
-    }
+    // if (this.cate !== "") {
+    //   this.answersList = this.chatService.getAnswers(this.cate)
+    //   this.quizsAndAnswersList = this.chatService.getQuizAndAnswers()
+    //   this.input = false;
+    //   this.selectCat = false;
+    // }
   }
 
   onSelectCategorie(categorie_id: any) {
     this.chatService.getCategorieThemeList(categorie_id.value);
   }
-  
 }
 
