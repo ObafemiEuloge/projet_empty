278bb65cebc88890b9fabf259c057e516703bdc9
Euloge <eagnoun@gmail.com>
2023-08-07 20:44:07 +0100

    Home page

--- src/app/animation.module.ts
+++ src/app/animation.module.ts
@@ -7,3 +7,9 @@ export const opacityAnimation = trigger('opacityAnimation', [
         animate('2000ms cubic-bezier(0.35, 0, 0.55, 1)', style({opacity: 1})),
     ]),
 ]);
+
+export const loadingMessage = trigger("loadingMessageAnimation", [
+    transition(":enter", [
+        style({})
+    ])
+])
