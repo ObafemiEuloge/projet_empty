cf388cbd463d85ee4f786d54543e85c57715b183
Euloge <eagnoun@gmail.com>
2023-08-08 18:04:39 +0100

    correction conflit

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { opacityAnimation } from '../animation.module';
-import { ChatService } from '../chat.service';
 
 @Component({
   selector: 'app-start',
@@ -9,11 +8,4 @@ import { ChatService } from '../chat.service';
   animations: [opacityAnimation]
 })
 export class StartComponent {
-  answersList: any[] | undefined;  // récupérer le tableau de réponses 
-  quizsAndAnswersList: any[] | undefined;  // récupérer le tableau de (questions et réponses)
-
-  constructor(
-    private chatService: ChatService
-  ) { }
-  
 }
