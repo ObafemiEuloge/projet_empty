1fa8a1c8de587ef01229aa067d451b73fda7a180
Euloge <eagnoun@gmail.com>
2023-08-08 17:40:46 +0100

    Responsive et correction

--- src/app/start-bot/start-bot.component.spec.ts
+++ src/app/start-bot/start-bot.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { StartComponent } from './start.component';
+import { StartBotComponent } from './start-bot.component';
 
-describe('StartComponent', () => {
-  let component: StartComponent;
-  let fixture: ComponentFixture<StartComponent>;
+describe('StartBotComponent', () => {
+  let component: StartBotComponent;
+  let fixture: ComponentFixture<StartBotComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [StartComponent]
+      declarations: [StartBotComponent]
     });
-    fixture = TestBed.createComponent(StartComponent);
+    fixture = TestBed.createComponent(StartBotComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/start-bot/start-bot.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-bot',
+  templateUrl: './start-bot.component.html',
+  styleUrls: ['./start-bot.component.css']
+})
+export class StartBotComponent {
+
+}
